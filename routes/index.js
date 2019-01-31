a44b94612213693dd91e4961a99d7804c94760b5
Loc Truong <l4truong@ucsd.edu>
2019-01-31 01:37:52 -0800

    node.js lab

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,28 @@
  */
 
 exports.view = function(req, res){
-  res.render('index');
+   res.render('index', {
+    'projects': [
+      { 'name': 'Postmates Redesign Interactive Prototype',
+        'image': 'lorempixel.people.1.jpeg',
+        'id': 'project1'
+      },
+      { 'name': 'NFL Math',
+        'image': 'lorempixel.city.1.jpeg',
+        'id': 'project2'
+      },
+      { 'name': 'Blue Sheep',
+        'image': 'lorempixel.technics.1.jpeg',
+        'id': 'project3'
+      },
+      { 'name': 'CoupleTones',
+        'image': 'lorempixel.abstract.1.jpeg',
+        'id': 'project4'
+      },
+      { 'name': 'Tetris Program',
+        'image': 'lorempixel.abstract.8.jpeg',
+        'id': 'project5'
+      }
+    ]  
+  });
 };
